2866c1404df95de526ae724d53ae143ff90096c0
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-09-17 02:29:54 +0100

    c-cpp: add LDFLAGS to final targets linking

--- c-cpp/src/c-example.c
+++ c-cpp/src/c-example.c
@@ -1,12 +1,29 @@
 #include "example.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *strdup(const char *source) {
+  return strcpy(malloc(strlen(source) + 1), source);
+}
 
 int main(void) {
-  const size_t n = 6;
+  {
+    const char *string = "Hello world from c!";
+    char *dupStr = strdup(string);
 
-  printf("Hello world from c!\n");
-  printf("%zu! = %zu\n", n, factorial(n));
+    printf("string = %s\n", string);
+    printf("dupStr = %s\n", dupStr);
+
+    // Memory leak
+  }
+
+  {
+    const size_t n = 6;
+
+    printf("%zu! = %zu\n", n, factorial(n));
+  }
 
   return 0;
 }
--- c-cpp/targets.mk
+++ c-cpp/targets.mk
@@ -6,10 +6,10 @@ DEPS := src/c-example.c src/example.c
 DEPS := $(DEPS:src/%.c=$(BUILD_DIR)/%.o)
 $(TARGET_DIR)/c-example: $(DEPS)
 	@mkdir --parents $(shell dirname $@)
-	$(CC) -o $@ $^
+	$(CC) -o $@ $^ $(LDFLAGS)
 
 DEPS := src/cpp-example.cpp
 DEPS := $(DEPS:src/%.cpp=$(BUILD_DIR)/%.o)
 $(TARGET_DIR)/cpp-example: $(DEPS)
 	@mkdir --parents $(shell dirname $@)
-	$(CXX) -o $@ $^
+	$(CXX) -o $@ $^ $(LDFLAGS)
